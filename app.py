301365ebb22b3b04ccff76f4634ae0b7c42d36d8
npsytoh <npsytoh@gmail.com>
2024-09-17 22:58:41 +0900

    fix: app.py

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ if page == 'users':
     st.title('User add')
 
     with st.form(key='user'):
-        # user_id: int = random.randint(0, 10)
         user_name: str = st.text_input('User name', max_chars=12)
         data = {
-            # 'user_id': user_id,
             'user_name': user_name
         }
         submit_button = st.form_submit_button(label='Add')
@@ -29,29 +27,25 @@ if page == 'users':
         st.json(res.json())
 
 elif page == 'rooms':
-    st.title('API Test(Rooms)')
+    st.title('Room add')
 
     with st.form(key='room'):
-        room_id: int = random.randint(0, 10)
         room_name: str = st.text_input('Room name', max_chars=12)
         capacity: int = st.number_input('Capacity', step=1)
         data = {
-            'room_id': room_id,
             'room_name': room_name,
             'capacity': capacity
         }
-        submit_button = st.form_submit_button(label='Send')
+        submit_button = st.form_submit_button(label='Add')
 
     if submit_button:
-        st.write('## Send data')
-        st.json(data)
-        st.write('## Response')
         url = 'http://127.0.0.1:8000/rooms'
         res = requests.post(
             url,
             data=json.dumps(data)
         )
-        st.write(res.status_code)
+        if res.status_code == 200:
+            st.success('Room add success')
         st.json(res.json())
 
 elif page == 'bookings':
